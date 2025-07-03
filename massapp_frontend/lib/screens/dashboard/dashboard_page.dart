1c1bb30682fcfb325d9e9783af559609da0c2c32
Septian Jauhariansyah <septian.j@gmail.com>
2025-07-04 05:14:34 +0700

    fixing dashboard page

--- massapp_frontend/lib/screens/dashboard/dashboard_page.dart
+++ massapp_frontend/lib/screens/dashboard/dashboard_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Register Page')),
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Dashboard Page'),
+      ),
     );
   }
 }
